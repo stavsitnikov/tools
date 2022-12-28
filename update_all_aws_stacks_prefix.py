daed97a121ed3a024028dad2fd2655a16908612c
Stav Sitnikov <stav.sitni@gmail.com>
2022-12-28 21:37:31 +0200

    update all stacks with a specific prefix across all regions in a specific aws account

---
+++ update_all_aws_stacks_prefix.py
@@ -0,0 +1,53 @@
+import boto3
+import tqdm
+from botocore.exceptions import ClientError
+
+# Set up the CloudFormation client , use this in case you have an account connected via aws configure cli
+cfn_client = boto3.client('cloudformation')
+
+
+# Get the list of all regions
+regions = [region['RegionName'] for region in boto3.client('ec2').describe_regions()['Regions']]
+
+#set CloudFormation stackname prefix
+prefix = "-lightlytics-"
+
+#set CloudFormation prefix of nested stack to ingore it
+n1prefix = "-LightlyticsCollectionLambdas-"
+
+#set CloudFormation prefix of nested stack to ingore it
+n2prefix = "-LightlyticsInitLambdas-"
+
+# Iterate over each region
+for region in regions:
+    # Set up a new CloudFormation client for the current region
+    cfn_client = boto3.client('cloudformation', region_name=region)
+
+    # Get the list of stacks in the region
+    stacks = cfn_client.list_stacks()['StackSummaries']
+
+    # Filter the list of stacks to only include a specific prefix and status is complete create or update complete, and name not contains LightlyticsInitLambdas
+
+    stacks = [stack for stack in stacks if (prefix in stack['StackName'] and (stack['StackStatus'] == 'CREATE_COMPLETE' or stack['StackStatus'] == 'UPDATE_COMPLETE')) and not n1prefix in stack['StackName']and not n2prefix in stack['StackName']]
+    
+    #pring all found stacks names
+    print([stack['StackName'] for stack in stacks])
+
+    # Iterate over each stack and update it
+    for stack in tqdm.tqdm(stacks, desc=f"Updating stacks in {region}"): 
+        stack_name = stack['StackName']
+
+        try:            
+         # Update the stack using the existing template, UsePreviousValue required parameters from running template Parameters:[LightlyticsApiUrl, LightlyticsAwsAccountId, RegionsToDeploy, ExternalId, AccountAuthToken, TemplateVersion, LightlyticsInternalAccountId] add capabilities=['CAPABILITY_NAMED_IAM'] to the update_stack call if you are using IAM roles in your template       
+             cfn_client.update_stack(StackName=stack_name, UsePreviousTemplate=True)    
+       # Wait for the update to complete
+             cfn_client.get_waiter('stack_update_complete').wait(StackName=stack_name)
+   
+            # Print the name of the stack that was successfully updated
+             print(f"Successfully updated stack {stack_name} in {region}")
+        except ClientError as e:
+ #         # Print an error message if the stack no longer exists
+            print(f"Failed to update stack {stack_name} in {region}: {e}")
+
+     
+
