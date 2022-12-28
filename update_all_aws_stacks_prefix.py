e2e086a327a1979b5766e71a28a3c1621b880aa3
Stav Sitnikov <stav.sitni@gmail.com>
2022-12-28 21:49:25 +0200

    added colors for prints

--- update_all_aws_stacks_prefix.py
+++ update_all_aws_stacks_prefix.py
@@ -1,10 +1,10 @@
 import boto3
 import tqdm
+import termcolor
 from botocore.exceptions import ClientError
 
 # Set up the CloudFormation client , use this in case you have an account connected via aws configure cli
 cfn_client = boto3.client('cloudformation')
-
 
 # Get the list of all regions
 regions = [region['RegionName'] for region in boto3.client('ec2').describe_regions()['Regions']]
@@ -36,18 +36,17 @@ for region in regions:
     # Iterate over each stack and update it
     for stack in tqdm.tqdm(stacks, desc=f"Updating stacks in {region}"): 
         stack_name = stack['StackName']
-
         try:            
-         # Update the stack using the existing template
+            # Update the stack using the existing template
              cfn_client.update_stack(StackName=stack_name, UsePreviousTemplate=True)    
-       # Wait for the update to complete
+
+             # Wait for the update to complete
              cfn_client.get_waiter('stack_update_complete').wait(StackName=stack_name)
-   
+
             # Print the name of the stack that was successfully updated
-             print(f"Successfully updated stack {stack_name} in {region}")
+             print(termcolor.colored(f"Successfully updated stack {stack_name} in {region}", "green"))
+
         except ClientError as e:
- #         # Print an error message if the stack no longer exists
-            print(f"Failed to update stack {stack_name} in {region}: {e}")
 
-     
-
+            # Print an error message if the stack no longer exists
+             print(termcolor.colored(f"Failed to update stack {stack_name} in {region}: {e}", "red"))
